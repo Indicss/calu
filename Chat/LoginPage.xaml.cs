8bd88b7d1f1707347764f7ce188f442903a1e826
Jalba Oleg <jalbaoleg00@gmail.com>
2024-07-22 23:51:16 +0300

    bai, am adaugat restu

---
+++ Chat/LoginPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Maui.Controls;
+using System.Collections.ObjectModel;
+
+namespace Chat
+{
+    public partial class LoginPage : ContentPage
+    {
+        public ObservableCollection<string> Messages { get; set; }
+
+        public LoginPage()
+        {
+            InitializeComponent();
+            Messages = new ObservableCollection<string>();
+            MessagesCollectionView.ItemsSource = Messages;
+        }
+
+        private void OnSendButtonClicked(object sender, EventArgs e)
+        {
+            string message = MessageEntry.Text;
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                Messages.Add(message);
+                MessageEntry.Text = string.Empty; // Golește câmpul de text după trimitere
+                MessagesCollectionView.ScrollTo(Messages.Count - 1, -1, ScrollToPosition.End, true); // Derulează la ultimul mesaj
+            }
+        }
+    }
+}
---
+++ Chat/MainPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Maui.Controls;
+using Chat;
+
+namespace Chat
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnCounterClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new LoginPage());
+        }
+    }
+}
